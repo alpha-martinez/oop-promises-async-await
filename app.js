6c2e7f5a114fb2efa23e0a7305cb9c4ea6081c9e
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-23 18:53:40 -0400

    using constructor & fetch on our github

--- app.js
+++ app.js
@@ -80,7 +80,73 @@ function Person(first, last, age, gender) {
     this.lastName = last;
     this.age = age;
     this.gender = gender;
+    this.intro = function() {
+        console.log("Hi, my name is " + this.firstName);
+    }
 }
 
+
+
 let anna = new Person('Anna', 'Martinez', 20, 'Female');
+let alpha = new Person('Alpha', 'Martinez', 20, 'Female');
 console.log(anna);
+anna.intro(); //invoke
+
+alpha.intro();
+
+
+//make a constructor function
+//make 3 variables with that constructor function
+
+//class...constructor is what you pass through your class//constructor is the info
+// you put in and drive is what you put out
+
+class Car {
+    constructor(year, make, model, color) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+    }
+
+    drive() {
+        console.log('Vroom');
+    }
+    
+    intro() {
+        console.log('This car is a ' + this.make + ' ' + this.model);
+    }
+}
+let tesla = new Car (2020, 'Tesla', 'Model S', 'red');
+console.log(tesla);
+tesla.drive();
+tesla.intro();
+
+
+class GithubProfile {
+    constructor(username, name, url) {
+        this.username = username;
+        this.name = name;
+        this.url = url;
+    }
+    intro() {
+        console.log(`My name is ${this.name} and my username is @${this.username}`)
+    }
+}
+
+
+
+fetch('https://api.github.com/users/alpha-martinez')
+.then (response => {
+    return response.json();
+ })
+.then(data =>{
+    //console.log(data);
+    let githubURL = data.url;
+   // console.log(githubUrl);
+   let githubUsername = data.login;
+   let githubName = data.name;
+
+   let alpha = new GithubProfile(githubUsername, githubName, githubURL);
+   alpha.intro();
+})
