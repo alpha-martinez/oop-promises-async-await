c4819f074e2eb4d9ed592eae0209f0b207172f40
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-23 17:45:42 -0400

    [constructor] person

--- app.js
+++ app.js
@@ -47,5 +47,40 @@ console.log(apartOne.rooms);
 
 //constructor functions
 
+// let User = (name) => { //when using this function, use a capital letter in the begining
+//     this.name = name;   //return this
+// } doesn't work this way
 
 
+
+function User(name) {
+    this.name = name;
+}
+
+
+let adam = new User('Adam');
+let pete = new User('pete');
+
+console.log(adam.name);
+console.log(pete.name);
+
+function NBAPlayer(name, team, threePointShooter) {
+    //this is equal to an object --> this = {}
+    this.name = name;
+    this.team = team;
+    this.threePointShooter = threePointShooter;
+}
+
+let steph = new NBAPlayer('Steph Curry', 'Warriors', true);
+console.log(steph);
+
+
+function Person(first, last, age, gender) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.gender = gender;
+}
+
+let anna = new Person('Anna', 'Martinez', 20, 'Female');
+console.log(anna);
